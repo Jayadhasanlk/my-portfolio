e43235203058aff37529964487a3871d56aea4a6
jayadhasan2003-blip <jayadhasan2003@gmail.com>
2026-08-21 09:41:24 +0530

    Initial commit

---
+++ app/views.py
@@ -0,0 +1,23 @@
+from django.shortcuts import render
+
+
+def home(request):
+    return render(request, "home.html")
+
+
+def about(request):
+    return render(request, "about.html")
+
+
+def projects(request):
+    return render(request, "projects.html")
+
+
+def contact(request):
+    return render(request, "contact.html")
+
+def static(request):
+    return render(request,"index.html")
+
+def p(request):
+    return render(request,"index2.html")
